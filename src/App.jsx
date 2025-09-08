90ada55355520950fa74827533316eba46e5079d
samikshyapattnaik <samikshyapattnaik14@gmail.com>
2025-09-08 18:12:06 +0530

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import About from './components/About';
 import Business from './components/Business';
+import Team from './components/Team';
+import Footer from './components/Footer';
 function App() {
 
   return (
@@ -12,6 +14,8 @@ function App() {
       <Hero/>
       <About/>
       <Business/>
+      <Team/>
+      <Footer/>
       <Routes>
         <Route path="/" />
 
