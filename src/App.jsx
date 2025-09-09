815b9aafcd930fc93a28b39e7690f95ba11a8307
samikshyapattnaik <samikshyapattnaik14@gmail.com>
2025-09-09 18:36:16 +0530

    add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { react,useEffect,useState } from 'react'
 import { Routes, Route } from "react-router-dom";
 import Header from './components/Header'
 import Hero from './components/Hero'
@@ -6,21 +6,25 @@ import About from './components/About';
 import Business from './components/Business';
 import Team from './components/Team';
 import Footer from './components/Footer';
+import Connect from './components/Connect';
+import toast, { Toaster } from "react-hot-toast";
 function App() {
-
+   
   return (
     <>
+      <Toaster position="top-center" reverseOrder={false} /> 
       <Header/>
       <Hero/>
       <About/>
       <Business/>
       <Team/>
+      <Connect/>
       <Footer/>
       <Routes>
         <Route path="/" />
 
       </Routes>
-    </>
+        </>
   )
 }
 
