0817cf75c65918deed02abec66eba03888096db6
samikshyapattnaik <samikshyapattnaik14@gmail.com>
2025-09-10 18:29:44 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,49 @@
 import { react,useEffect,useState } from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Header from './components/Header'
-import Hero from './components/Hero'
 import About from './components/About';
 import Business from './components/Business';
 import Team from './components/Team';
 import Footer from './components/Footer';
 import Connect from './components/Connect';
+import Hero from './components/Hero'
+import Blog from './components/Blog';
 import toast, { Toaster } from "react-hot-toast";
-function App() {
-   
+function HomePage() {
   return (
     <>
-      <Toaster position="top-center" reverseOrder={false} /> 
-      <Header/>
-      <Hero/>
-      <About/>
-      <Business/>
-      <Team/>
-      <Connect/>
-      <Footer/>
-      <Routes>
-        <Route path="/" />
+      <section id="home">
+        <Hero />
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="business">
+        <Business />
+      </section>
+      <section id="team">
+        <Team />
+      </section>
+      <section id="connect">
+        <Connect />
+      </section>
+    </>
+  );
+}
 
+function App() {
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/blog" element={<Blog />} />
       </Routes>
-        </>
+
+      <Footer />
+    </>
   )
 }
 
